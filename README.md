a470a8817a0ecfc4e3d9ccb21a2b4cb242c0034b
Sittichai Chaikamol <nu@mcp.local>
2020-03-05 01:49:10 -0600

    test

--- README.md
+++ README.md
@@ -1,23 +1,13 @@
-![](https://avatars2.githubusercontent.com/u/57165288?v=4)
-        ##Name
-        Nu  
-        ##title
-        Good README Generator  
-        ##Description
-        This project uses to generate a readme file and Pdf of github profile  
-        ##TableOfContent
-        project tile, installation, github profile and usernam, and email address.  
-        ##Installation
-        npm  
-        ##Usage
-          
-        ##Lincse
-        undefined  
-        ##Contributin
-        Nu  
-        ##Test
-          
-        ##Profile
-        undefined  
-        ##Email
-        undefined
+(https://avatars2.githubusercontent.com/u/57165288?v=4)
+      
+  Name:Nu  
+        title:Good README Generator.  
+        Description:This project uses to generate a readme file and Pdf of github profile.  
+        Table Of Content:project title, installation, github profile and usernam, and email address.  
+        Installation:npm  
+        Usage:n/a  
+        Lincse:undefined  
+        Contributin:Nu  
+        Test:.  
+        Profile:undefined  
+        Email:undefined
--- index.js
+++ index.js
@@ -70,29 +70,18 @@ inquirer.prompt([
         console.log(res.data);
         var avatarImg = res.data.avatar_url;
 
-        fs.writeFile("README.md",  `![](${avatarImg})
-        ##Name
-        ${response.name}  
-        ##title
-        ${response.title}  
-        ##Description
-        ${response.description}  
-        ##TableOfContent
-        ${response.tableOfContent}  
-        ##Installation
-        ${response.installation}  
-        ##Usage
-        ${response.usage}  
-        ##Lincse
-        ${response.lincse}  
-        ##Contributin
-        ${response.contributing}  
-        ##Test
-        ${response.test}  
-        ##Profile
-        ${response.profile}  
-        ##Email
-        ${response.email}`, function (err) {
+        fs.writeFile("README.md",`(${avatarImg})
+      \n  Name:${response.name}  
+        title:${response.title}  
+        Description:${response.description}  
+        Table Of Content:${response.tableOfContent}  
+        Installation:${response.installation}  
+        Usage:${response.usage}  
+        Lincse:${response.lincse}  
+        Contributin:${response.contributing}  
+        Test:${response.test}  
+        Profile:${response.profile}  
+        Email:${response.email}`, function (err) {
 
             if (err) {
                return console.log(err);
@@ -104,3 +93,4 @@ inquirer.prompt([
         console.log(err);
     });
 });
+
